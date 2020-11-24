d1143ae9e9d0522533308de5bb5953bfb3951eb7
hzqmwne <huangzhengqmwne@sina.cn>
2020-11-24 23:28:46 +0800

    update two tests

--- test-lab3-part2-b.cc
+++ test-lab3-part2-b.cc
@@ -15,6 +15,7 @@ using namespace std;
 	} \
 } while(0)
 
+/*
 #define CHECK_TRANS_EXCEPTION(express, expected_exception, errmsg) do { \
 	bool exception_occurred = false; \
 	try { \
@@ -25,13 +26,17 @@ using namespace std;
 	} \
 		CHECK(exception_occurred, errmsg); \
 } while(0)
-
+*/
 
 int global_err_count = 0;
 
 volatile int global_counter = 0;
 
 string global_ydb_server_port;
+
+bool t0_aborted = false;
+bool t1_aborted = false;
+bool t2_aborted = false;
 
 void *start_thread0(void *arg_) {
 	(void)arg_;
@@ -41,22 +46,18 @@ void *start_thread0(void *arg_) {
 
 	y.transaction_begin();
 	y.set("a", "0100");
-	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 3);
-	y.set("b", "0200");
-	while(global_counter < 4);
-	y.transaction_commit();
-	
-	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 7);
 
-	y.transaction_begin();
-	r = y.get("a");
-		CHECK(r == "0100", "tread0 check 1 error");
-	r = y.get("b");
-		CHECK(r == "0200", "tread0 check 2 error");
-	r = y.get("c");
-		CHECK(r == "1300", "tread0 check 3 error");
-	y.transaction_commit();
-	
+	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 3);
+
+	try {
+		y.set("b", "0200");
+		y.transaction_commit();
+	}
+	catch(ydb_protocol::status e) {
+		CHECK(e == ydb_protocol::ABORT, "check t0 abort error");
+		t0_aborted = true;
+	}
+
 	return NULL;
 }
 
@@ -68,21 +69,17 @@ void *start_thread1(void *arg_) {
 
 	y.transaction_begin();
 	y.set("b", "1200");
-	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 3);
-	y.set("c", "1300");
-	while(global_counter < 4);
-	y.transaction_commit();
 	
-	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 7);
-
-	y.transaction_begin();
-	r = y.get("a");
-		CHECK(r == "0100", "tread1 check 1 error");
-	r = y.get("b");
-		CHECK(r == "0200", "tread1 check 2 error");
-	r = y.get("c");
-		CHECK(r == "1300", "tread1 check 3 error");
-	y.transaction_commit();
+	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 3);
+	
+	try {
+		y.set("c", "1300");
+		y.transaction_commit();
+	}
+	catch(ydb_protocol::status e) {
+		CHECK(e == ydb_protocol::ABORT, "check t1 abort error");
+		t1_aborted = true;
+	}
 
 	return NULL;
 }
@@ -95,23 +92,17 @@ void *start_thread2(void *arg_) {
 
 	y.transaction_begin();
 	y.set("c", "2300");
+
 	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 3);
-	usleep(1000000/10);    // wait trans0 and trans1 getting in lock
-	CHECK_TRANS_EXCEPTION(y.set("a", "2100"), ydb_protocol::ABORT, "thread2 check abort error");
-	__sync_fetch_and_add(&global_counter, 1);
-	// it is already aborted, so no need to y.transaction_commit();
-	// now global_counter should be 4
-
-	__sync_fetch_and_add(&global_counter, 1); while(global_counter < 7);
-
-	y.transaction_begin();
-	r = y.get("a");
-		CHECK(r == "0100", "tread2 check 1 error");
-	r = y.get("b");
-		CHECK(r == "0200", "tread2 check 2 error");
-	r = y.get("c");
-		CHECK(r == "1300", "tread2 check 3 error");
-	y.transaction_commit();
+	
+	try {
+		y.set("a", "2100");
+		y.transaction_commit();
+	}
+	catch(ydb_protocol::status e) {
+		CHECK(e == ydb_protocol::ABORT, "check t2 abort error");
+		t2_aborted = true;
+	}
 
 	return NULL;
 }
@@ -144,6 +135,36 @@ int main(int argc, char **argv) {
 	pthread_join(t0, NULL);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+
+	y.transaction_begin();
+	string r1 = y.get("a");
+	string r2 = y.get("b");
+	string r3 = y.get("c");
+	y.transaction_commit();
+
+	// we should not assume which thread to abort (the old version of this test assumes the last transaction to abort, which is wrong)
+	// but, we can know there is one and should only one transaction aborted
+	
+	//printf("%d %d %d\n", t0_aborted, t1_aborted, t2_aborted);
+	
+	if (t0_aborted && !t1_aborted && !t2_aborted) {    // only transaction 0 aborted
+		CHECK(r1 == "2100", "t0 abort check a error");
+		CHECK(r2 == "1200", "t0 abort check b error");
+		CHECK(r3 == "1300", "t0 abort check c error");
+	}
+	else if (!t0_aborted && t1_aborted && !t2_aborted) {    // only transaction 1 aborted
+		CHECK(r1 == "2100", "t1 abort check a error");
+		CHECK(r2 == "0200", "t1 abort check b error");
+		CHECK(r3 == "2300", "t1 abort check c error");
+	}
+	else if (!t0_aborted && !t1_aborted && t2_aborted) {    // only transaction 2 aborted
+		CHECK(r1 == "0100", "t2 abort check a error");
+		CHECK(r2 == "0200", "t2 abort check b error");
+		CHECK(r3 == "1300", "t2 abort check c error");
+	}
+	else {
+		CHECK(false, "check only one transaction abort error");
+	}
 
 	if (global_err_count != 0) {
 		printf("[x_x] Fail test-lab3-part2-b\n");
--- test-lab3-part3-b.cc
+++ test-lab3-part3-b.cc
@@ -45,38 +45,44 @@ int main(int argc, char **argv) {
 	y1.set("a", "100");
 	y1.set("b", "200");
 	y1.set("c", "300");
-	y1.transaction_commit();
+	y1.transaction_commit();    // we allow false aborting between concurrency transactions, but non-concurrency transactions should never abort
 
 	y1.transaction_begin();
 	y2.transaction_begin();
 	y3.transaction_begin();
 
+	r = y1.get("a");
+		CHECK(r == "100", "check 1 error");
 	y1.set("a", "1100");
+	r = y2.get("b");
+		CHECK(r == "200", "check 2 error");
 	y2.set("b", "2100");
+	r = y3.get("c");
+		CHECK(r == "300", "check 3 error");
 	y3.set("c", "3100");
 
 	r = y1.get("b");
-		CHECK(r == "200", "check 1 error");
+		CHECK(r == "200", "check 4 error");
 	y1.set("b", "1200");
 	r = y2.get("c");
-		CHECK(r == "300", "check 2 error");
+		CHECK(r == "300", "check 5 error");
 	y2.set("c", "2200");
 	r = y3.get("a");
-		CHECK(r == "100", "check 3 error");
+		CHECK(r == "100", "check 6 error");
 	y3.set("a", "3200");
 
-	y3.transaction_commit();
-	CHECK_TRANS_EXCEPTION(y2.transaction_commit(), ydb_protocol::ABORT, "check 4 error");
-	CHECK_TRANS_EXCEPTION(y1.transaction_commit(), ydb_protocol::ABORT, "check 5 error");
+	y3.transaction_commit();    // we allow false aborting between concurrency transactions, but the first commit of concurrency transactions aborts seems impossible
+	CHECK_TRANS_EXCEPTION(y2.transaction_commit(), ydb_protocol::ABORT, "check 7 error");    // here it must abort
+	CHECK_TRANS_EXCEPTION(y1.transaction_commit(), ydb_protocol::ABORT, "check 8 error");    // here it must abort
 
 
 	y2.transaction_begin();
 	r = y2.get("a");
-		CHECK(r == "3200", "check 6 error");
+		CHECK(r == "3200", "check 9 error");
 	r = y2.get("b");
-		CHECK(r == "200", "check 7 error");
+		CHECK(r == "200", "check 10 error");
 	r = y2.get("c");
-		CHECK(r == "3100", "check 8 error");
+		CHECK(r == "3100", "check 11 error");
 	y2.transaction_commit();	
 
 	
